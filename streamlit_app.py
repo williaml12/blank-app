75aad5361ce55e94eb2cd88659904bff6e5265b0
William Lu <32563160+williaml12@users.noreply.github.com>
2024-07-22 13:54:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@
 #     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 # )
 
+import streamlit as st
+
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
@@ -25,5 +27,4 @@ contact_form = f"""
             <button type="submit">Send</button>
         </form>
         """
-        
 st.markdown(contact_form, unsafe_allow_html=True)
