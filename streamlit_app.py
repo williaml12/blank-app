8ba5c78a55ee5b0fba78f6dac8374c0baa73a705
William Lu <32563160+williaml12@users.noreply.github.com>
2024-07-22 13:41:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,23 +6,7 @@
 #     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 # )
 
-import requests
 import streamlit as st
-
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-# Apply local CSS styles from the "style.css" file
-local_css("style/style.css")
-
-lottie_contact = load_lottieurl("https://lottie.host/6c502d7d-9573-4d15-8063-b93dd8aef2af/MhPNlv4ZJ5.json")
 
 contact_form = f"""
 <form action="https://formsubmit.co/alphagalaga@gmail.com" method="POST">
@@ -34,7 +18,6 @@ contact_form = f"""
 </form>
 """
 
-
+# Displaying the form using Streamlit
 st.subheader("📨 Contact Me")
 st.markdown(contact_form, unsafe_allow_html=True)
-
