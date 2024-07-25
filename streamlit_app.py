b83adf61029e30c877eb6aa1dbcf72d5a1142caf
William Lu <32563160+williaml12@users.noreply.github.com>
2024-07-25 10:32:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,5 @@
 #     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 # )
 
-# <a href="https://www.example.com">This is a link</a>
-[This is a link](https://www.example.com)
+<a href="https://www.example.com">education</a>
 
