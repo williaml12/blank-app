dc78273f02870a74062909b5cbc0d9a8decf4238
William Lu <32563160+williaml12@users.noreply.github.com>
2024-07-28 12:40:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ st.markdown("""
         box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
     }
     .project-title {
-        font-size: 18px;
+        font-size: 16px;
         font-weight: bold;
         margin-top: 10px;
     }
