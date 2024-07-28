2db701d01c50b359b8efb46f68a921746d532d16
William Lu <32563160+williaml12@users.noreply.github.com>
2024-07-28 12:34:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,6 @@ st.markdown("""
         margin: 10px;
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
         transition: 0.3s;
-        text-align: center;
     }
     .project-card:hover {
         box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
